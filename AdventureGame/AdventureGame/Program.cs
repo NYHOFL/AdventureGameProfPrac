cf3e4b2656f50e2358918c5c526d8e5c393e1fb2
Caleb <arphonage@gmail.com>
2019-06-02 14:39:23 +1200

    Continuing on with the story

--- AdventureGame/AdventureGame/Program.cs
+++ AdventureGame/AdventureGame/Program.cs
@@ -40,20 +40,39 @@ namespace AdventureGame
                 MurderCards[i].murderWeapon = weaponArray[rand.Next(0, 6)];
                 MurderCards[i].murderRoom   = roomArray[rand.Next(0, 9)];
             }
-            Console.ReadLine();
 
         }
         public static void Game()
         {
-            Console.WriteLine("The buzz of your cellphone interrupts a friendly game of croquchet. The sun beams through the tall pine trees, a sweet smell in the air. As chief police investigator a murder scene isn't unknown to you.");
+            string temp;
+
+            Console.WriteLine($"The buzz of your cellphone interrupts a friendly game of croquchet. The sun beams through the tall pine trees, a sweet smell in the air. As chief police investigator a murder scene isn't unknown to you.\n");
             Thread.Sleep(2000);
-            Console.WriteLine("Will you discover the murderer? Or will they escape your grasp?");
+            Console.WriteLine("Press enter to continue");
+            Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine($"Will you discover the murderer? Or will they escape your grasp? \n");
             Thread.Sleep(2000);
+            Console.WriteLine("Press enter to continue");
+            Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine();
             Console.WriteLine("Loading...");
             Thread.Sleep(4000);
             Console.Clear();
-            Console.WriteLine(" Arriving on the police cordoned scene you discover six suspects and a large masion.The dirty marble pillars outside hold the pearly white roof up.You escort the suspects into the cellar in the middle of the mansion, down the stairs and onto a old wooden bench.You begin thequestioning.");
 
+            Console.WriteLine("Arriving on the police cordoned scene you discover six suspects and a large masion.The dirty marble pillars outside hold the pearly white roof up.You escort the suspects into the cellar in the middle of the mansion, down the stairs and onto a old wooden bench.You begin thequestioning.");
+            Thread.Sleep(2000);
+            Console.ReadLine();
+
+            Console.Clear();
+
+            Console.WriteLine("Interact or leave"); // this is the first menu that will ask the player what they want to do 
+            temp = Console.ReadLine();
+            // if interact, go to a list of the suspects. if leave, exit to the mansion.
+            
         }
 
         static void Main()
